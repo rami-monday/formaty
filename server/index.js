3ea3f97e67b78d87aa5af1ba3397601df5752086
rami-monday <rami@monday.com>
2022-07-29 11:41:52 +0300

    adding mongoose

--- server/index.js
+++ server/index.js
@@ -2,10 +2,21 @@ const express = require("express");
 const router = require("./routes");
 const app = express();
 const cors = require("cors");
-const port = 3000;
+const mongoose = require("mongoose");
+require("dotenv").config();
 
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("connected to db"))
+  .catch(() => console.log("couldn't connect to db"));
+  
 app.use(express.json());
 app.use(cors());
 app.use(router);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(PORT, () =>
+  console.log(`formaty server listening on port ${PORT}!`)
+);
