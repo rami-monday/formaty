a82eb914ea530dc812f98c387feca4a84dd2eadc
rami-monday <rami@monday.com>
2022-07-29 11:36:20 +0300

    server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
-const express = require('express')
-const router = require('./routes')
-const app = express()
-const port = 3000
+const express = require("express");
+const router = require("./routes");
+const app = express();
+const cors = require("cors");
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.json());
+app.use(cors());
+app.use(router);
 
-app.use(router)
-
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
