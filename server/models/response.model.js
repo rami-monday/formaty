70452f338abaf9442004774e6712b0bea9ecdb8a
iMac 2 <imac2@iMac2.local>
2022-07-29 12:20:11 +0300

    finished

--- server/models/response.model.js
+++ server/models/response.model.js
@@ -1,21 +1,10 @@
 const { response } = require('express')
 const mongoose = require('mongoose')
-
 const  Schema =  mongoose.Schema
 const ResponseSchema = new Schema({
     id : String,
     formId : String,
     inputValues : String
 })
-
 const Response = mongoose.model('response',ResponseSchema)
-
 module.exports = Response
-
-
-
-
-
-
-
-
