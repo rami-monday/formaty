2733b7be4a5c8ed8dc0a5993e655785730fcd2f5
YazeedSal <yazeedsalameh02082005@gmail.com>
2022-08-05 10:26:51 +0300

    fixed the  code

--- src/services/user.js
+++ src/services/user.js
@@ -2,18 +2,18 @@ import axios from "axios";
 
 const url = "http://localhost:3001/";
 
-export const SignInApiManager = async function () {
+export const SignInApiManager = async function (user) {
   try {
-    const response = await axios.get(`${url}/user/signIn`);
+    const response = await axios.get(`${url}/user/signIn`,user);
     return response.data
   } catch (error) {
     alert(JSON.stringify(error));
   }
 };
 
-export const SignUpApiManager = async function () {
+export const SignUpApiManager = async function (user) {
     try {
-      const response = await axios.post(`${url}/user/signUp`);
+      const response = await axios.post(`${url}/user/signUp`,user);
       return response.data
     } catch (error) {
       alert(JSON.stringify(error));
